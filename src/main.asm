9535c8ad87e3dbc94f2c16eefd5523d26687fddd
ATcat-pl <antektyczka@gmail.com>
2025-06-13 23:34:50 +0200

    basic video mode switching

--- src/main.asm
+++ src/main.asm
@@ -2,19 +2,26 @@
 BITS 16
 
 section .data
-	msg	db	"Hello world", '$'
+	old_video_mode	resb 1  ; variable to store old video mode to return to
 
 section .text
 	global _start
 
 _start:
-	mov	ax, seg msg
-	mov	ds, ax
-
-	mov	ah, 0x09
-	mov	dx, msg
-	int	0x21
+set_video_mode:
+	mov	ah, 0x0F
+	int	0x10
+	mov	[old_video_mode], al ; save current video mode
 	
+	mov	ah, 0x00
+	mov	al, 0x13 ; 320x200@8bits
+	int	0x10	
+
+reset_video_mode:
+	mov	ah, 0x00
+	mov	al, [old_video_mode]
+	int	0x10
+exit:	
 	mov	ah, 0x4C
 	mov	al, 0
 	int	0x21
